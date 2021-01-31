3d61bc87572dc015843233f4c183e01781da7776
Filip Fey <filipfey@gmail.com>
2021-01-31 15:37:35 +0100

    adds content to home page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <h1>J. Doe's Personal APP</h1>
-    <router-link to="/">Home</router-link>
-    <router-link to="/contact-me">Contact Me</router-link>
+    <h1 class="title">J. Doe's Personal APP</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/contact-me">Contact Me</router-link>
+    </nav>
     <router-view />
   </div>
 </template>
 <style>
-#app {
+nav {
+  text-align: center;
+}
+.title {
   text-align: center;
 }
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,36 @@
 <template>
   <div id="home">
-    <h2>Home Page</h2>
+    <h2 class="title">Filip Fey</h2>
+    <div class="profile-picture">
+      <img alt="My profile picture" src="../assets/my-picture.jpg" />
+    </div>
+    <div class="block-text">
+      <p>
+        I am a mathematician and web developer specialized in JS frontend
+        technologies. I am into web development for 5 years now and have 2 years
+        of work experience as frontend developer in corporate environment.
+      </p>
+      <p>
+        I can quickly switch between all 3 mainstream frontend frameworks
+        (Angular, Vue and React) with Vue being my favorite and React the most
+        recent I used. At first, I was learning PHP while creating one of my
+        personal projects and that's when I gained a solid understanding of SQL
+        database structures and other stuff backend developers are concerned
+        about. This knowledge gives me a broader picture of projects I work on,
+        occasionally gets me some valuable insight and improves quality of
+        communication with backend developers I work with.
+      </p>
+      <p>
+        I value excellence, learning, teamwork and love for what I am doing.
+        Improving myself and using that to improve others is what I think good
+        life is about. I actively nurture these values so it is important to me
+        that the company I am a part of nurtures these values as well.
+      </p>
+    </div>
   </div>
 </template>
-<style>
-#home {
+<style scoped>
+.profile-picture {
   text-align: center;
-  color: red;
 }
 </style>
