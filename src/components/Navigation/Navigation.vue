1d98bbf80280d5fb5c389f58b63c5630586ca62f
Filip Fey <filipfey@gmail.com>
2021-02-01 09:44:41 +0100

    includes vue-burger-menu into navigation for tablets and smaller

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,14 +1,27 @@
 <template>
   <nav>
-    <router-link v-for="link in links" :key="link.to" :to="link.to">{{
-      link.title
-    }}</router-link>
+    <div class="navigation--mobile">
+      <Slide :close-on-navigation="true">
+        <router-link v-for="link in links" :key="link.to" :to="link.to">{{
+          link.title
+        }}</router-link>
+      </Slide>
+    </div>
+    <div class="navigation--desktop">
+      <router-link v-for="link in links" :key="link.to" :to="link.to">{{
+        link.title
+      }}</router-link>
+    </div>
   </nav>
 </template>
 <script>
 import _ from 'lodash'
+import { Slide } from 'vue-burger-menu'
 import { Link } from './types'
 export default {
+  components: {
+    Slide
+  },
   props: {
     links: {
       type: Array,
@@ -20,3 +33,17 @@ export default {
   }
 }
 </script>
+<style scoped>
+.navigation--desktop {
+  display: none;
+}
+@media screen and (min-width: 992px) {
+  .navigation--mobile {
+    display: none;
+  }
+
+  .navigation--desktop {
+    display: block;
+  }
+}
+</style>
