44746b401c30548513dbbcde3a3619949c816882
Filip Fey <filipfey@gmail.com>
2021-02-01 09:01:34 +0100

    including simple navigation component into App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
     <h1 class="title">J. Doe's Personal APP</h1>
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/contact-me">Contact Me</router-link>
-    </nav>
+    <Navigation :links="links" />
     <router-view />
   </div>
 </template>
+<script>
+import { Navigation } from './components'
+import { links } from './constants'
+export default {
+  components: {
+    Navigation
+  },
+  data() {
+    return {
+      links
+    }
+  }
+}
+</script>
 <style>
 nav {
   text-align: center;
