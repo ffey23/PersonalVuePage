0e2b1cf2483f82aad7db500e0eb14e780673c5e3
Filip Fey <filipfey@gmail.com>
2021-02-01 09:01:07 +0100

    creating simple navigation component

---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <router-link v-for="link in links" :key="link.to" :to="link.to">{{
+      link.title
+    }}</router-link>
+  </nav>
+</template>
+<script>
+import _ from 'lodash'
+import { Link } from './types'
+export default {
+  props: {
+    links: {
+      type: Array,
+      required: true,
+      validator: function (links) {
+        return _.every(links, (link) => link instanceof Link)
+      }
+    }
+  }
+}
+</script>
