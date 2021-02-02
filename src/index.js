d8a9839c57aafb4d859dc24e7c23c42a90614b51
Filip Fey <filipfey@gmail.com>
2021-02-02 16:04:24 +0100

    including swal 2 vue plugin into project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import routes from './routes'
 import axios from './plugins/axios'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 const router = new VueRouter({
   routes,
   mode: 'history',
@@ -11,6 +13,7 @@ const router = new VueRouter({
 
 Vue.use(VueRouter)
 Vue.use(axios)
+Vue.use(VueSweetalert2)
 
 new Vue({
   el: '#app',
